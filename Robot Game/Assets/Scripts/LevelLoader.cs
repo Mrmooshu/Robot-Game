828eb5fa411991d2140675417552318619c8173e
Mrmooshu <mrmooshu3@hotmail.com>
2021-10-29 07:51:44 -0400

    scenes and transitions
    -added start menu scene
    -added transitions between scenes

---
+++ Robot Game/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1;
+
+    public void SelectScene(int sceneIndex)
+    {
+        StartCoroutine(LoadScene(sceneIndex));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
